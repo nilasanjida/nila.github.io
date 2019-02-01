82b4462ef59c2cf1c8d00889f5fbe4cab9a13555
Nila <sanjidakabirnila95@users.noreply.github.com>
2019-02-01 16:46:15 +0600

    My website

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+	<title>Home (Hotel Management)</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link href="image/LOGO.gif" type="image/icon" rel="icon">
+</head>
+<body>
+	<div id="full"> 
+		<div style="background-image: url('image/b1.jpg'); width: 100%;height: 600px">
+		<div id="header">
+			<div id="logo">
+				<h1><font color="white">My Project</font></h1>
+			</div>
+			<div id="nav">
+				<ul>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">Contect Us</a></li>
+					<li><a href="#">BOOK MY STAY</a></li>
+					<li><a href="#">Our Hotels</a></li>
+					<li><a href="#">Help</a></li>
+				</ul>
+				
+			</div>
+		</div>	
+		<div id="banner"></div>	
+	</div>
+    </div>
+</div>
+
+</body>
+</html>	
